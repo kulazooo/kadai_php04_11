27cecd55efed4525be3d37c255f10d80f4896c30
kulazooo <kulapro11@gmail.com>
2022-02-05 10:58:56 +0900

    v3

--- chatwork-api.php
+++ chatwork-api.php
@@ -1,7 +1,7 @@
 <?php
     header("Content-type: text/html; charset=utf-8");
      
-    $api_key = "597220aeaf345b47fb5db2ff642d3b84"; // APIキー
+    $api_key = "*****"; // APIキー
     $room_id = 260664689; // ルームID
     $url = "https://api.chatwork.com/v2/rooms/249432049/messages"; // API URL
      
