70018bd389e7dd5841b2a4f6884809c0cb28c47b
kulazooo <kulapro11@gmail.com>
2022-02-05 22:02:21 +0900

    v4

--- logout.php
+++ logout.php
@@ -14,5 +14,5 @@ if (isset($_COOKIE[session_name()])) { //session_name()は、セッションID�
 session_destroy();
 
 //処理後、index.phpへリダイレクト
-header("Location: index.php");
+header("Location: login.php");
 exit();
